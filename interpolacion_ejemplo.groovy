965e7824eec1751a4b8d89214fe5dc4fc5df34c0
David Torres <d.torres015@ebc.edu.mx>
2020-05-19 12:18:18 -0500

    Se agrega ejemplo de interpolación en los GString Groovy

---
+++ interpolacion_ejemplo.groovy
@@ -0,0 +1,5 @@
+str1= "Jim"
+str2 = "Hello "
+//Puede o no llevar {} ya que estas variables no hacen referencia a un marcador por ejemplo a un elemento de un  objeto, en mi caso siempre usar {}
+println "${str2}${str1}"
+println "$str2$str1"
