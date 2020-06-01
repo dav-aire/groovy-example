6c4e0913c5f8c02e08165db6dd009db4af6c5bf8
David Torres <d.torres015@ebc.edu.mx>
2020-06-01 12:35:26 -0500

    Uso del operator .& para reutilizar metodos de un parametro como closure en Groovy

--- MethosAsAClosure_example.groovy
+++ MethosAsAClosure_example.groovy
@@ -1,3 +1,5 @@
+// El operatos .& solo se ha utilizado con metodos que reciban un solo parametro
+
 def list = ["David", "Jazmin", "Elena"]
 list.each{
     println "${this.class.name} ${it}"
@@ -7,7 +9,17 @@ println ""
 
 String printName(String name){
     println "${this.class.name}"
-    println name
+    println "$name"
 }
 
 printName("David") // Ejecuta el metodo
+
+list.each(this.&printName) // Se utiliza el operador .& para accesar al metodo printName como si fuera un closure
+
+println ""
+
+def diHola = {
+    println it
+}
+
+list.each(diHola)
